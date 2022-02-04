5cd57961b134761835ce0734b741f23b964cf01f
Dmitry <dmitrij.euseew@yandex.ru>
2022-02-04 21:58:53 +0300

    add function

--- ConsoleApplication11/ConsoleApplication11.cpp
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << add(4, 5) << endl;
 }
